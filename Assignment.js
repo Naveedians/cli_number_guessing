e4a086b84fe544ddf03836b9fc084f476404a605
Naveed Ahmed <160510766+Naveedians@users.noreply.github.com>
2024-03-28 06:02:22 +0500

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,9 @@
+"use strict";
+//class assignment
+//function
+//return
+//parameters and argument
+function add(a, b, c) {
+    return a + b + c;
+}
+console.log(add(10, 20, 4)); //call function
---
+++ Assignment.ts
@@ -0,0 +1,11 @@
+
+//class assignment
+//function
+//return
+//parameters and argument
+
+function add(a:number,b:number,c:number)//in this code we write function in pass parameters
+{
+return a+b+c;
+}
+console.log(add(10,20,4));//call function
